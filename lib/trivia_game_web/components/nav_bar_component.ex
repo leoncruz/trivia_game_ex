d9d516b340475a5cbaa1f70557476a3496208d8d
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-08 23:07:34 -0300

    style: apply style to authentication pages

--- lib/trivia_game_web/components/nav_bar_component.ex
+++ lib/trivia_game_web/components/nav_bar_component.ex
@@ -1,18 +1,28 @@
 defmodule TriviaGameWeb.Components.NavBarComponent do
   use Phoenix.Component
   use Phoenix.HTML
+  alias TriviaGameWeb.Router.Helpers, as: Routes
 
   def nav(assigns) do
     ~H"""
     <nav class="h-2 w-screen md:h-16 shadow-lg">
       <div class="h-full invisible md:visible flex items-center justify-between mx-16">
-        <%= Phoenix.HTML.Link.link "Home", to: "#" %>
+        <%= link "Home", to: Routes.page_path(@conn, :index) %>
 
         <h1 class="text-4xl">Trivia Game</h1>
 
         <div class="flex flex-row space-x-8 items-center">
-          <%= link "Sign In", to: "#" %>
-          <%= link "Sign Up", to: "#", class: "py-2 px-4 rounded bg-violet-300 hover:bg-violet-400" %>
+          <%= if @current_user do %>
+            <%= link "Settings", to: Routes.user_settings_path(@conn, :edit) %>
+            <%= link "Log out", to: Routes.user_session_path(@conn, :delete), method: :delete,
+              class: "py-2 px-4 rounded text-white bg-violet-500 hover:bg-violet-400 active:bg-violet-400"
+            %>
+          <% else %>
+            <%= link "Sign In", to: Routes.user_session_path(@conn, :new) %>
+            <%= link "Sign Up", to: Routes.user_registration_path(@conn, :new),
+              class: "py-2 px-4 rounded text-white bg-violet-500 hover:bg-violet-400 active:bg-violet-400"
+            %>
+          <% end %>
         </div>
       </div>
 
@@ -26,9 +36,18 @@ defmodule TriviaGameWeb.Components.NavBarComponent do
 
       <div id="mobile-menu-list" class="invisible mt-4 shadow-lg">
         <ul class="flex flex-col space-y-8">
-          <li class="py-2 px-4"><%= link "Home", to: "#" %></li>
-          <li class="py-2 px-4"><%= link "Sign In", to: "#" %></li>
-          <li class="py-2 px-4 bg-violet-300 "><%= link "Sign Up", to: "#" %></li>
+          <li class="py-2 px-4"><%= link "Home", to: Routes.page_path(@conn, :index) %></li>
+          <%= if @current_user do %>
+            <li class="py-2 px-4"><%= link "Settings", to: Routes.user_settings_path(@conn, :edit) %></li>
+            <li class="py-2 px-4 text-white bg-violet-500 hover:bg-violet-400 active:bg-violet-400 ">
+              <%= link "Log out", to: Routes.user_session_path(@conn, :delete) %>
+            </li>
+          <% else %>
+            <li class="py-2 px-4"><%= link "Sign In", to: Routes.user_session_path(@conn, :new) %></li>
+            <li class="py-2 px-4 text-white bg-violet-500 hover:bg-violet-400 active:bg-violet-400 ">
+              <%= link "Sign Up", to: Routes.user_registration_path(@conn, :new) %>
+            </li>
+          <% end %>
         </ul>
       </div>
     </nav>
