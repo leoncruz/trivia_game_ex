85f3732507ca6cf239825504de4b899d59a93701
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-03-18 00:08:45 -0300

    save user answer and go to next question

--- lib/trivia_game_web/live/game_live/index.ex
+++ lib/trivia_game_web/live/game_live/index.ex
@@ -3,22 +3,53 @@ defmodule TriviaGameWeb.GameLive.Index do
 
   alias TriviaGame.Game
 
+  alias Phoenix.LiveView.JS
+
   def mount(%{"category" => category_id}, _session, socket) do
-    questions = Game.get_questions(category_id)
+    case connected?(socket) do
+      true ->
+        questions = Game.get_questions(category_id)
 
-    {first_question, questions} = remove_question(questions)
+        {first_question, questions} = Game.next_question(questions)
 
-    socket =
-      socket
-      |> assign(:questions, questions)
-      |> assign(:current_question, first_question)
+        socket =
+          socket
+          |> assign(:questions, questions)
+          |> assign(:current_question, first_question)
+          |> assign(:loading, false)
 
-    {:ok, socket}
+        {:ok, socket}
+
+      false ->
+        {:ok, assign(socket, :loading, true)}
+    end
   end
 
-  defp remove_question(questions) do
-    [first_question | rest] = questions
+  def handle_params(_params, _uri, socket) do
+    {:noreply, socket}
+  end
 
-    {first_question, rest}
+  def handle_event("respond", params, socket) do
+    %{"answer" => answer, "question" => question} = params
+
+    %{current_user: current_user, questions: questions} = socket.assigns
+
+    Game.respond(current_user.id, question, answer)
+
+    case Game.next_question(questions) do
+      :finish ->
+        {:noreply,
+         push_patch(socket,
+           to: Routes.game_index_path(socket, :index)
+         )}
+
+      {next_question, question_rest} ->
+        socket =
+          socket
+          |> assign(:questions, question_rest)
+          |> assign(:current_question, next_question)
+
+        {:noreply, socket}
+    end
   end
 end
